c3c0213d3abc921d070776172cd8108bbe421aa3
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-03-06 16:36:01 +0400

    Added expo router

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({});
