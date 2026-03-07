c235e8f3345fc4a7ed5b9e91f14eefc32ebaabe9
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-03-07 23:04:46 +0400

    Map changes

--- app/index.jsx
+++ app/index.jsx
@@ -63,6 +63,8 @@ const Main = () => {
                     width: "100%",
                     height: "100%",
                 }}
+                toolbarEnabled={false}
+                showsCompass={false}
                 initialRegion={{
                     latitude: 37.78825,
                     longitude: -122.4324,
